56f8e8871549be801219cb4933d69a28c4772405
IvanB <baklanovivan25@gmail.com>
2024-12-03 21:39:10 +0500

    Authentication templates, registration and user login forms have been created, updated urls.py , added views

---
+++ src/authentication/forms.py
@@ -0,0 +1,37 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.models import User
+
+class RegisterUserForm(UserCreationForm):
+    error_messages = {
+        "password_mismatch": "The two password field didn't match.",
+    }
+
+    username = forms.CharField(
+        label='Username', 
+        widget=forms.TextInput(attrs={'class': 'form-input'}),
+        error_messages={'unique': "This username is already taken."},
+        )
+    password1 = forms.CharField(
+        label='Password', 
+        widget=forms.PasswordInput(attrs={'class': 'form-input'}),
+        help_text="Password must be at least 8 characters long and contain both letters and numbers.",
+        )
+    password2 = forms.CharField(
+        label='Repeat Password', 
+        widget=forms.PasswordInput(attrs={'class': 'form-input'}),
+        ) 
+
+    class Meta:
+        model = User
+        fields = ('username', 'password1', 'password2')
+
+class LoginUserForm(AuthenticationForm):
+    username = forms.CharField(
+        label='Username', 
+        widget=forms.TextInput(attrs={'class': 'form-input'}),
+        )
+    password = forms.CharField(
+        label='Password', 
+        widget=forms.PasswordInput(attrs={'class': 'form-input'}),
+        )
--- src/authentication/views.py
+++ src/authentication/views.py
@@ -1,3 +1,47 @@
-from django.shortcuts import render
+from django.shortcuts import redirect
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+from django.contrib.auth import login
+from django.contrib.auth.views import LoginView, LogoutView
 
-# Create your views here.
+from .forms import RegisterUserForm, LoginUserForm
+
+
+class RegisterUserView(CreateView):
+    form_class = RegisterUserForm
+    # TODO: добавить шаблон
+    template_name = "..."
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["title"] = "Register"
+        return context
+
+    def form_valid(self, form):
+        user = form.save()
+        login(self.request, user)
+        return redirect("home")
+
+    def get_success_url(self):
+        return reverse_lazy("home")
+
+
+class LoginUserView(LoginView):
+    form_class = LoginUserForm
+    # TODO: добавить шаблон
+    template_name = "..."
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["title"] = "Authorization"
+        context["username"] = (
+            self.request.user.username if self.request.user.is_authenticated else None
+        )
+        return context
+
+    def get_success_url(self):
+        return reverse_lazy("home")
+
+
+class LogoutUserView(LogoutView):
+    next_page = reverse_lazy("home")
