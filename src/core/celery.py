916ab7ef7a1e3467ac075f6c3b9ee119e130ffcb
IvanB <baklanovivan25@gmail.com>
2025-08-06 03:58:35 +0500

    Fixed docker-compose and tasks for working celery

--- src/core/celery.py
+++ src/core/celery.py
@@ -1,20 +1,14 @@
 from __future__ import absolute_import, unicode_literals
-import os
 
 from celery import Celery
 from celery.schedules import crontab
-
-# Set the default Django settings module for the 'celery' program.
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings.development")
-
-from django.conf import settings
 
 app = Celery("core")
 
 app.conf.beat_schedule = {
     "delete-old-anonymous-carts-everyday": {
         "task": "cart.tasks.delete_old_anonymous_carts",
-        "schedule": crontab(hour=0, minute=0),
+        "schedule": crontab(hour=0, minute=0),  # Ежедневно в полночь
     },
 }
 
@@ -27,3 +21,8 @@ app.config_from_object("django.conf:settings", namespace="CELERY")
 
 # Load task modules from all registered Django apps.
 app.autodiscover_tasks()
+
+# Отладка
+# @app.task(bind=True)
+# def debug_task(self):
+#     print(f'Request: {self.request!r}')
--- src/core/settings/development.py
+++ src/core/settings/development.py
@@ -50,3 +50,5 @@ AUTHENTICATION_BACKENDS = [
     "axes.backends.AxesStandaloneBackend",
     "django.contrib.auth.backends.ModelBackend",
 ]
+
+SITE_URL = "http://127.0.0.1:8000"
