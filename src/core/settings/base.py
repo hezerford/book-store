d090cd092f5b28f6e2415845ee5f26db9928a09c
IvanB <baklanovivan25@gmail.com>
2025-09-14 00:38:57 +0500

    Added django-elasticsearch-dsl package to poetry and settings

--- src/core/settings/base.py
+++ src/core/settings/base.py
@@ -24,6 +24,7 @@ INSTALLED_APPS = [
     "axes",
     "drf_spectacular",
     "imagekit",
+    "django_elasticsearch_dsl",
     # apps
     "store.apps.StoreConfig",
     "authentication.apps.AuthenticationConfig",
