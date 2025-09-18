a5b65b5a36417e52846421904828ae3279fcc442
IvanB <baklanovivan25@gmail.com>
2025-09-18 19:08:17 +0500

    Refactor version API endpoints to v1 and remove unused debug toolbar code

--- src/core/urls.py
+++ src/core/urls.py
@@ -7,26 +7,21 @@ from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
     path("", include("store.urls")),
-    path("api/", include("api.urls")),
     path("auth/", include("authentication.urls")),
     path("cart/", include("cart.urls")),
     path("profile/", include("user_profile.urls")),
     path("admin/", admin.site.urls),
     path("captcha/", include("captcha.urls")),
-    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    # API
+    path("api/v1/", include("api.urls")),
+    path("api/v1/schema/", SpectacularAPIView.as_view(), name="schema"),
     path(
         "api/v1/docs/",
         SpectacularSwaggerView.as_view(url_name="schema"),
         name="swagger-ui",
     ),
 ]
-# ] + debug_toolbar_urls()
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-#     import debug_toolbar
-
-#     urlpatterns += [
-#         path(r"^__debug__/", include(debug_toolbar.urls)),
-#     ]
